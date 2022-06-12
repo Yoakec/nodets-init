8a1c8dc7384ddf2fc326a55981f5c83a0acc1379
Yoakec <1602491057@qq.com>
2022-06-12 15:55:14 +0800

    fix mysql

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,3 +1,11 @@
+/*
+ * @Author: Yoakec
+ * @Date: 2022-05-26 10:04:58
+ * @LastEditors: Yoakec
+ * @LastEditTime: 2022-06-12 15:53:52
+ * @FilePath: \node_ts\src\data-source.ts
+ * @Description: 
+ */
 import "reflect-metadata"
 import { DataSource } from "typeorm"
 import { Post } from "./entity/Post"
@@ -17,3 +25,14 @@ export const AppDataSource = new DataSource({
     migrations: ["src/migration/**/*.ts"],
     subscribers: ["src/subscriber/**/*.ts"]
   })
+
+  // to initialize initial connection with the database, register all entities
+// and "synchronize" database schema, call "initialize()" method of a newly created database
+// once in your application bootstrap
+AppDataSource.initialize()
+.then(() => {
+    console.log("Data Source has been initialized!")
+})
+.catch((err) => {
+    console.error("Error during Data Source initialization", err)
+})
